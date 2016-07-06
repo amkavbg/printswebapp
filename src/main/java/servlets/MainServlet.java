4d390a5b23d733f86d8e072bb895ef376dbe2c28
amkavbg <amka.vbg@gmail.com>
2016-07-06 14:25:28 +0300

    ftl ready table

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -44,23 +44,18 @@ public class MainServlet extends HttpServlet {
         cfg.setDefaultEncoding("UTF-8");
         cfg.setIncompatibleImprovements(new Version(2, 3, 20));
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setObjectWrapper(ObjectWrapper.BEANS_WRAPPER);
         //  Load template
         Template tpl = cfg.getTemplate("src/main/resources/templates/printers.tpl");
         //  Data model
         Map<String, Object> input = new HashMap<String, Object>();
         input.put("title", "PRINTSWEBAPP 0.2a by tokido");
         input.put("tablename", "œ–»Õ“≈–€ ÷œ");
-
         Map<String, Object> map = new HashMap<>();
-
         for (Printer printer : pmap.values()) {
                 map.put(printer.getIp()+" - "+printer.getValueByKey("NetName"),printer.getParameters());
         }
-
-        //input.put("printers", map);
-
-//      System.out.println("In method DOGET printer map: " + pmap.size() + "\n " + pmap);
-
+        input.put("printers", map);
         response.setContentType("text/html;charset=utf-8");
             try {
                 tpl.process(input,response.getWriter());
--- src/main/resources/templates/printers.tpl
+++ src/main/resources/templates/printers.tpl
@@ -6,11 +6,16 @@
 
 <body>
     <center><h2>${tablename}</h2></center>
-
     <center>
-        <#list printers as prt>
-            <p>${prt.ip}</p>
-        </#list>
+
+    <#list printers.entrySet() as entry>
+            <caption>${entry.key}</caption>
+        <table border="1">
+                <#list entry.value.entrySet() as param>
+                    <tr><td>${param.key}<td>${param.value}
+                </#list>
+        </table>
+    </#list>
 
     </center>
 
