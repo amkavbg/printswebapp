c3132579d5d9f00ad2e422ab3108e1f71ac96b05
amkavbg <amka.vbg@gmail.com>
2016-07-08 16:34:30 +0300

    ready for jetty:docker

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -18,21 +18,35 @@ import java.util.Map;
  */
 public class MainServlet extends HttpServlet {
 
-    private Map<String, Printer> pmap = new HashMap<>();
+//private Map<String, Printer> pmap = new HashMap<>();
     private Engine eng;
+    private Template tpl;
 
     @Override
     public void init() throws ServletException {
+        //  Configure freemarker template
+        Configuration cfg = new Configuration();
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setIncompatibleImprovements(new Version(2, 3, 20));
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setObjectWrapper(ObjectWrapper.BEANS_WRAPPER);
+        cfg.setClassForTemplateLoading(this.getClass(), "/" + "templates");
 
-        ServletContext context = getServletContext();
-        String realPath = context.getRealPath("/WEB-INF/classes/");
-
-        Engine eng = new Engine(realPath);
         try {
-           pmap = eng.getPrintersInfo();
+            tpl = cfg.getTemplate("printers.tpl");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        ServletContext context = getServletContext();
+        String realPath = context.getRealPath("/WEB-INF/classes/");
+        this.eng = new Engine(realPath);
+
+//        try {
+//           pmap = eng.getPrintersInfo();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -45,31 +59,30 @@ public class MainServlet extends HttpServlet {
             throws ServletException, IOException {
 
         //  Configure
-        Configuration cfg = new Configuration();
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setIncompatibleImprovements(new Version(2, 3, 20));
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        cfg.setObjectWrapper(ObjectWrapper.BEANS_WRAPPER);
-        cfg.setClassForTemplateLoading(this.getClass(),"/"+"templates");
-        //  Load template
-        Template tpl = cfg.getTemplate("printers.tpl");
-        //  Data model
-        Map<String, Object> input = new HashMap<String, Object>();
+//        Configuration cfg = new Configuration();
+//        cfg.setDefaultEncoding("UTF-8");
+//        cfg.setIncompatibleImprovements(new Version(2, 3, 20));
+//        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+//        cfg.setObjectWrapper(ObjectWrapper.BEANS_WRAPPER);
+//        cfg.setClassForTemplateLoading(this.getClass(),"/"+"templates");
+//        //  Load template
+//        Template tpl = cfg.getTemplate("printers.tpl");
 
+        //  Data model
+
+        Map<String, Object> input = new HashMap<String, Object>();
         input.put("title", "PRINTSWEBAPP 0.2a by tokido");
         input.put("tablename", "œ–»Õ“≈–€ ÷œ");
+        Map<String, Object> printermap = new HashMap<>();
 
-        Map<String, Object> map = new HashMap<>();
-
-        for (Printer printer : pmap.values()) {
-                map.put(printer.getIp()+" - "+printer.getValueByKey("NetName"),printer.getParameters());
+        for (Printer printer : eng.getPrintersInfo().values()) {
+                printermap.put(printer.getIp() + " - " + printer.getValueByKey("NetName"), printer.getParameters());
         }
-
-        input.put("printers", map);
-
+        input.put("printers", printermap);
         response.setContentType("text/html;charset=utf-8");
+
             try {
-                tpl.process(input,response.getWriter());
+                this.tpl.process(input,response.getWriter());
             } catch (TemplateException e) {
                 e.printStackTrace();
             }
